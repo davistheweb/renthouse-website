9388abe36e9d42e0a55bd10a1bd8cadecf44067f
davistheweb <smartkelvin775@gmail.com>
2024-03-19 04:54:42 +0100

    done with reviews area

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -101,23 +101,25 @@ if (navigator.connection) {
     
     
 
-        let swiperCards = new Swiper(".slide-review", {
+       /*  const swiper = new Swiper('.swiper', {
+            // Optional parameters
+            direction: 'vertical',
             loop: true,
-            spaceBetween:50,
-            grabCursor: true,
-         
-            navigation: {
-              nextEl: ".swiper-button-next",
-              prevEl: ".swiper-button-prev",
+          
+            // If we need pagination
+            pagination: {
+              el: '.swiper-pagination',
             },
           
-            breakpoints:{
-              600: {
-                slidesPerView: 2,
-              },
-              968: {
-                slidesPerView: 3,
-              },
+            // Navigation arrows
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
             },
-          });
+          
+            // And if we need scrollbar
+            scrollbar: {
+              el: '.swiper-scrollbar',
+            },
+          }); */
 //END!//
