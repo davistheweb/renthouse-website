511b95a423dc057e2767f85e231afcf0009c8a82
davistheweb <smartkelvin775@gmail.com>
2024-03-19 03:55:32 +0100

    new chnages

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -100,4 +100,24 @@ if (navigator.connection) {
         });
     
     
+
+        let swiperCards = new Swiper(".slide-review", {
+            loop: true,
+            spaceBetween:50,
+            grabCursor: true,
+         
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+          
+            breakpoints:{
+              600: {
+                slidesPerView: 2,
+              },
+              968: {
+                slidesPerView: 3,
+              },
+            },
+          });
 //END!//
